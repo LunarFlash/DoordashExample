9bc71128674e042d28a0c465893b59bf55b9c3e2
Yi Wang <logins.yiwang@gmail.com>
2022-02-19 22:08:30 -0500

    instantiate modifier automatically

--- DoorDashExperiments/Controller/AddModifierTableViewController.swift
+++ DoorDashExperiments/Controller/AddModifierTableViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class AddModifierTableViewController: UITableViewController {
   
-  var modifier: Modifier?
+  var modifier: Modifier? = Modifier(name: "")
   
   @IBAction func didTapSave(_ sender: Any) {
     didSaveModifier?(modifier)
@@ -30,7 +30,6 @@ class AddModifierTableViewController: UITableViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
   }
-  
   
   // MARK: - Table view data source
   override func numberOfSections(in tableView: UITableView) -> Int {
@@ -80,41 +79,6 @@ class AddModifierTableViewController: UITableViewController {
   }
   
   /*
-   // Override to support conditional editing of the table view.
-   override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-   // Return false if you do not want the specified item to be editable.
-   return true
-   }
-   */
-  
-  /*
-   // Override to support editing the table view.
-   override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-   if editingStyle == .delete {
-   // Delete the row from the data source
-   tableView.deleteRows(at: [indexPath], with: .fade)
-   } else if editingStyle == .insert {
-   // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-   }
-   }
-   */
-  
-  /*
-   // Override to support rearranging the table view.
-   override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-   
-   }
-   */
-  
-  /*
-   // Override to support conditional rearranging of the table view.
-   override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-   // Return false if you do not want the item to be re-orderable.
-   return true
-   }
-   */
-  
-  /*
    // MARK: - Navigation
    
    // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -144,9 +108,6 @@ extension AddModifierTableViewController {
             let price = Double(priceStr) else { return }
       print("title:\(modiferTitle) price:\(price)")
       let option = ModifierOption(name: modiferTitle, price: price)
-      if self?.modifier == nil {
-        self?.modifier = Modifier(name: "")
-      }
       self?.modifier?.options.append(option)
       
       self?.tableView.reloadData()
